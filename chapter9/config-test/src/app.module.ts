1b7fd73d566c9bfac98740c17407da3f17a6c583
manlubo <manlubo11@gmail.com>
2025-10-09 22:32:31 +0900

    환경변수 설정

---
+++ chapter9/config-test/src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import {ConfigModule} from "@nestjs/config";
+import { WeatherModule } from './weather/weather.module';
+
+console.log('env : ' + process.env.NODE_ENV);
+console.log("현재 디렉토리 : " + process.cwd());
+console.log(`${process.cwd()}/envs/${process.env.NODE_ENV}.env`);
+
+@Module({
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: `${process.cwd()}/envs/${process.env.NODE_ENV}.env`,
+  }), WeatherModule,],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
